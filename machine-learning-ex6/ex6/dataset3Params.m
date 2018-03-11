3643c91cf42f286df1b1c6b3853371750426c2dd
yuki678 <uk.12juni@gmail.com>
2018-03-11 20:13:00 +0000

    ex6 completed

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% CVec = [0.01;0.03;0.1;0.3;1;3;10;30;100;300];
+% sigmaVec = [0.01;0.03;0.1;0.3;1;3;10;30;100;300];
 
+% error = zeros(size(CVec,1), size(sigmaVec,1));
 
+% for i = 1:size(CVec,1)
+%     C = CVec(i)
+%     for j = 1:size(sigmaVec,1)
+%         sigma = sigmaVec(j)
+%         model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+%         error(i,j) = mean(double(svmPredict(model, Xval) ~= yval));
+%     end
+% end
 
+% error
 
+% [M, I] = min(error(:))
+% [I_row, I_col] = ind2sub(size(error),I)
 
+% C = CVec(I_row)
+% sigma = sigmaVec(I_col)
+
+C=1
+sigma=0.1
 
 % =========================================================================
 
